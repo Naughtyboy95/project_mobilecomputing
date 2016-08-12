00729d7f25e81a1323488ded76aeee1835d084c5
Naughtyboy95 <phampeter620@gmail.com>
2016-08-12 12:13:19 +1000

    abc
    adsf

---
+++ Ducphatpham_Project1/Ducphatpham_Project1/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace Ducphatpham_Project1
+{
+	[Activity(Label = "DucphatphamProject1", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		private List<string> mItems;
+		private ListView mItems
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			// SetContentView (Resource.Layout.YourMainView);
+		}
+	}
+}
+
+
